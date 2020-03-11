bb553ec1310e5f01d371f64a6e9147f96923f011
Nigol <rait.nigol@khk.ee>
2020-03-11 09:18:06 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+# käivita quartz brauser ja index.html fail
 sudo /usr/local/bin/quartz --kiosk /home/pi/ipcamera/index.html &
-unclutter --idle 5
+# kaota kursor peale 5 sekundit
+unclutter --idle 5 &
+# keela screensaver
+xset s 0 &
+xset -dpms &
+
 exit 0
