2dd63c3b83efad710244fbb2898ed2c1e35577fb
Nigol <rait.nigol@khk.ee>
2020-03-12 14:28:02 +0200

    webkit

--- js/script.js
+++ js/script.js
@@ -1,34 +1,15 @@
 // variabled
 var myHeaders, image, x, h, m, s, today
-
 // käivita funktsioonid (piltide allalaadimine ja reaalaja näitamine)
 function startFunctions() {
     setInterval(DownloadImage, 150);
     showTime();
-
-    // openweathermap api, et saada temperatuur tartus
-    $(document).ready(function getWeather() {
-        $.getJSON("http://api.openweathermap.org/data/2.5/weather?q=tartu&appid=bc8e99bcfa56459251da8618f258d152&units=metric", function(result) {
-            console.log(result);    
-            var celsius, city, feelslike
-            celsius = result.main.temp;
-            city = result.name;
-            feelslike = result.main.feels_like;
-            document.getElementById("city").innerHTML = `${city}`;
-            document.getElementById("celsius").innerHTML = `Temperatuur: ${celsius} C`; 
-            document.getElementById("feelslike").innerHTML = `Tundub nagu: ${feelslike} C`
-            // uuenda andmeid iga 3 min tagant
-            setTimeout(getWeather, 180000);
-        })
-    })
 }
 
 // tee uus objekt Headerite jaoks
 myHeaders = new Headers();
 myHeaders.set('Cache-Control', 'no-cache');
 myHeaders.set('Cache-Control', 'no-store');
-myHeaders.set('Cache-Control', 'max-age 0');
-myHeaders.set('Cache-Control', 'must-revalidate');
 
 // lae pildid alla ja käivita stream
 function DownloadImage() {
@@ -39,7 +20,7 @@ function DownloadImage() {
         x.src = image.src;
     };
 
-    image.src = "http://172.17.15.57/jpg/image.jpg?_=" + (+new Date());
+    image.src = "http://172.17.15.57/jpg/image.jpg" + "?_=" + (+new Date());
 }
 
 function showTime() {
